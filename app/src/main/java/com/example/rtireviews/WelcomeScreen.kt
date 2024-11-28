5717b89e3f89139c52e5938941479771a190f428
israelopeters <israelopeters@gmail.com>
2024-11-28 09:02:09 +0000

    feat(ui): Create App Details composable

---
+++ app/src/main/java/com/example/rtireviews/WelcomeScreen.kt
@@ -0,0 +1,84 @@
+package com.example.rtireviews
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.paddingFromBaseline
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.rtireviews.ui.theme.RTIReviewsTheme
+
+@Composable
+fun WelcomeScreen(
+    modifier: Modifier = Modifier
+) {
+
+    // App details
+
+    // Login CTA
+
+    // Sign-up CTA
+}
+
+@Composable
+fun AppDetails(
+    @DrawableRes image: Int,
+    @StringRes header: Int,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+    ) {
+        Text(
+            text = stringResource(header),
+            style = MaterialTheme.typography.headlineMedium,
+            modifier = modifier
+                .padding(top = 48.dp)
+                .paddingFromBaseline(
+                    top = 24.dp,
+                    bottom = 8.dp
+                )
+        )
+        Image(
+            painter = painterResource(id = image),
+            contentDescription = null,
+            alignment = Alignment.Center,
+            modifier = modifier
+                .padding(48.dp)
+                .clip(MaterialTheme.shapes.medium)
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun AppDetailsPreview() {
+    RTIReviewsTheme {
+        AppDetails(
+            R.drawable.welcome_screen_image,
+            R.string.welcome_screen_header
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun WelcomeScreenPreview() {
+    RTIReviewsTheme {
+        WelcomeScreen()
+    }
+}
