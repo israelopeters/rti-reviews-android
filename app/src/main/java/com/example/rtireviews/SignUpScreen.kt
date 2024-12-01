c6690fcaebb247bf5fabef1c7412d00d70370a4d
israelopeters <israelopeters@gmail.com>
2024-12-01 22:06:35 +0000

    feat(ui): Create top bar Composable for sign up screen

--- app/src/main/java/com/example/rtireviews/SignUpScreen.kt
+++ app/src/main/java/com/example/rtireviews/SignUpScreen.kt
@@ -4,18 +4,27 @@ import android.content.res.Configuration.UI_MODE_NIGHT_NO
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.rtireviews.ui.theme.RTIReviewsTheme
@@ -25,13 +34,15 @@ fun SignUpScreen(
     modifier: Modifier = Modifier
 ) {
     Scaffold(
-        // topBar = { SignUpScreenTopBar() },
+        topBar = { SignUpScreenTopBar() },
         modifier = modifier
     ) { padding ->
         Column(
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally,
-            modifier = Modifier.padding(padding).fillMaxSize()
+            modifier = Modifier
+                .padding(padding)
+                .fillMaxSize()
         ) {
             Spacer(Modifier.height(24.dp))
             // UserDetailsForm()
@@ -48,8 +59,26 @@ fun SignUpScreen(
 }
 
 @Composable
-fun SignUpScreenTopBar() {
-    TODO("Not yet implemented")
+fun SignUpScreenTopBar(modifier: Modifier = Modifier) {
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier.padding(16.dp).fillMaxWidth()
+    ) {
+        IconButton( onClick = {} ) {
+            Icon(
+                imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                contentDescription = stringResource(R.string.arrow_back)
+            )
+        }
+        Text(
+            text = stringResource(R.string.sign_up),
+            style = MaterialTheme.typography.titleLarge
+        )
+        TextButton( onClick = {} ) {
+            Text(stringResource(R.string.cancel))
+        }
+    }
 }
 
 @Composable
@@ -59,7 +88,7 @@ fun UserDetailsForm() {
 
 @Preview(
     showBackground = true,
-    showSystemUi = true,
+    //showSystemUi = true,
     uiMode = UI_MODE_NIGHT_NO,
     name = "DefaultPreviewLight"
 )
