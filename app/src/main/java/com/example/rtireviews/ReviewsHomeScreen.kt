8d9338b92ad0d2fca580811d45cf85a6374a023d
israelopeters <israelopeters@gmail.com>
2024-12-06 22:25:48 +0000

    feat: Create top app bar for home page

--- app/src/main/java/com/example/rtireviews/ReviewsHomeScreen.kt
+++ app/src/main/java/com/example/rtireviews/ReviewsHomeScreen.kt
@@ -1,8 +1,85 @@
 package com.example.rtireviews
 
+import android.content.res.Configuration.UI_MODE_NIGHT_NO
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.automirrored.filled.List
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.rtireviews.ui.theme.RTIReviewsTheme
 
 
 @Composable
-fun ReviewsHomeScreen(modifier: Modifier = Modifier) { }
+fun ReviewsHomeScreen(
+    modifier: Modifier = Modifier
+) {
+
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ReviewsHomeTopBar(
+    navigateUp: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    TopAppBar(
+        title = {
+            Text(
+                text = stringResource(R.string.reviews),
+                style = MaterialTheme.typography.titleLarge
+            )
+        },
+        navigationIcon = {
+            IconButton(onClick = navigateUp) {
+                Icon(
+                    imageVector = Icons.Filled.Menu,
+                    contentDescription = stringResource(R.string.menu)
+                )
+            }
+        },
+        colors = TopAppBarColors(
+            containerColor = MaterialTheme.colorScheme.surface,
+            scrolledContainerColor = MaterialTheme.colorScheme.surfaceContainerLow,
+            navigationIconContentColor = MaterialTheme.colorScheme.onSurface,
+            titleContentColor = MaterialTheme.colorScheme.onSurface,
+            actionIconContentColor = MaterialTheme.colorScheme.onSurface
+        ),
+        modifier = modifier
+            .padding(16.dp)
+            .fillMaxWidth()
+    )
+}
+
+@Preview(
+    showBackground = true,
+    uiMode = UI_MODE_NIGHT_NO,
+    name = "DefaultPreviewLight"
+)
+@Preview(
+    showBackground = true,
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "DefaultPreviewDark"
+)
+@Composable
+fun ReviewsHomeTopBarPreview() {
+    RTIReviewsTheme {
+        ReviewsHomeTopBar(
+            navigateUp = { }
+        )
+    }
+}
