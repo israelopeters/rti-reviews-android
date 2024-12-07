84942630847775ea82797ceba871ecc7107893ac
israelopeters <israelopeters@gmail.com>
2024-12-07 08:23:59 +0000

    feat: Add tabs to reviews home screen

--- app/src/main/java/com/example/rtireviews/ReviewsHomeScreen.kt
+++ app/src/main/java/com/example/rtireviews/ReviewsHomeScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.rtireviews.components.ReviewsHomeScreenTab
 import com.example.rtireviews.components.ReviewsHomeTopBar
 import com.example.rtireviews.ui.theme.RTIReviewsTheme
 
@@ -41,7 +42,7 @@ fun ReviewsHomeScreen(
         Column(
             modifier = Modifier.padding(innerPadding)
         ) {
-
+            ReviewsHomeScreenTab()
         }
 
     }
