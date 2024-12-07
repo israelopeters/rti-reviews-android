efb2ecc189d343624233488885072f5bbd21a5a3
israelopeters <israelopeters@gmail.com>
2024-12-07 08:15:56 +0000

    feat: Create tabs for reviews home screen

---
+++ app/src/main/java/com/example/rtireviews/components/ReviewHomeScreenTab.kt
@@ -0,0 +1,57 @@
+package com.example.rtireviews.components
+
+import android.content.res.Configuration.UI_MODE_NIGHT_NO
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.SecondaryTabRow
+import androidx.compose.material3.Tab
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.rtireviews.ui.theme.RTIReviewsTheme
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ReviewHomeScreenTab(
+    modifier: Modifier = Modifier
+) {
+    var state by remember { mutableStateOf(0) }
+    val titles = listOf("Explore", "Community")
+    Column {
+        SecondaryTabRow(
+            selectedTabIndex = state,
+        ) {
+            titles.forEachIndexed { index, title ->
+                Tab(
+                    selected = state == index,
+                    onClick = { state = index },
+                    text = { Text(text = title, maxLines = 2, overflow = TextOverflow.Ellipsis) }
+                )
+            }
+        }
+    }
+}
+
+@Preview(
+    showBackground = true,
+    uiMode = UI_MODE_NIGHT_NO,
+    name = "DefaultPreviewLight"
+)
+@Preview(
+    showBackground = true,
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "DefaultPreviewDark"
+)
+@Composable
+fun ReviewHomeScreenTabPreview() {
+    RTIReviewsTheme {
+        ReviewHomeScreenTab()
+    }
+}
